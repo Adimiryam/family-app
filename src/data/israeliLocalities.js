f0b76c8a02d519bb915c28a758e3238bf1ffd3d1
Adimiryam <adimiryam@gmail.com>
2026-03-27 18:40:40 +0300

    test: import parts 1+2 to check if build still works

--- src/data/israeliLocalities.js
+++ src/data/israeliLocalities.js
@@ -1,10 +1,12 @@
 import { LOCALITIES_PART1 } from './israeliLocalities_1'
+import { LOCALITIES_PART2 } from './israeliLocalities_2'
 
 export const SPECIAL_BASE = { name: 'בסיס כלשהו', lat: 32.7048, lng: 35.0271 }
 export const DEFAULT_LOCATION = { name: 'הכותל המערבי', lat: 31.7767, lng: 35.2345 }
 
 export const LOCALITIES = [
   ...LOCALITIES_PART1,
+  ...LOCALITIES_PART2,
 ]
 
 export const localityCoords = Object.fromEntries(
